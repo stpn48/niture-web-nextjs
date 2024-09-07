b8aae46fa6c7fa7914f17e0a09504f0b90e7a054
prdel24 <brozikmrozik7@gmail.com>
2024-09-07 23:22:07 +0200

    feat: create 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function NotFound({}: Props) {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <h1>404 Page Not Found.</h1>
+    </div>
+  );
+}
