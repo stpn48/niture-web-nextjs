5d9d15cd5e71f740f56415a2c05396fd9a052c49
prdel24 <brozikmrozik7@gmail.com>
2024-09-07 23:23:10 +0200

    feat: create Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+};
+
+export function Button({ children, onClick, className }: Props) {
+  return (
+    <button
+      className={`bg-[#262626] hover:bg-[#333333] active:bg-[#454545] text-white px-4 py-2 rounded-sm ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
