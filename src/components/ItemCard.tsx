cb7059d44cb9fe5f1907b225300dd79a3bea967c
prdel24 <brozikmrozik7@gmail.com>
2024-09-08 18:11:18 +0200

    Update ItemCard.tsx

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,15 +1,22 @@
 import { storeItem } from "@/app/types";
+import Image from "next/image";
 import React from "react";
 
 export function ItemCard({ item }: { item: storeItem }) {
   return (
-    <div className="min-w-[300px] h-[400px] bg-white rounded-lg shadow-md">
-      <img src={item.imgSrc} alt={item.alt} />
-      <div>
-        <h1>{item.name}</h1>
-        <h2>{item.description}</h2>
+    <div className="min-w-[300px] p-4 h-[400px] bg-white rounded-lg shadow-md">
+      <Image
+        width={300}
+        height={250}
+        objectFit="cover"
+        src={item.imgSrc}
+        alt={item.name}
+      />
+      <div className="mb-2">
+        <h1 className="font-bold mb-1">{item.name}</h1>
+        <p className="text-sm text-[#494949] text-justify">{item.description}</p>
       </div>
-      <h1>{item.price}</h1>
+      <h1 className="w-full flex justify-end pr-2">{item.price}</h1>
     </div>
   );
 }
