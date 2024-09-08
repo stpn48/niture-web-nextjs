748788f6bd2edac6bdf6c28ebcb1b72764e05738
prdel24 <brozikmrozik7@gmail.com>
2024-09-08 18:10:32 +0200

    feat: create api route for fetching collection details

--- src/app/api/collections/route.ts
+++ src/app/api/collections/route.ts
@@ -6,7 +6,7 @@ const COLLECTIONS = [
   {
     id: 1,
     name: "2024 Collection",
-    imgSrc: "/images/2024-collection.jpg", // TODO: the correct image path
+    imgSrc: "/2024collectionImg.jpg", // TODO: the correct image path
     alt: "2024 Collection Image",
     description:
       "Our 2024 Collection continues to refine and elevate living room design. With a deeper focus on comfort and modern aesthetics, this collection features sophisticated sofas, elegant media units, sleek coffee tables, and more. Designed to seamlessly fit into any modern home. Each piece in the 2024 Collection blends luxurious materials with minimalist design principles, bringing a sense of effortless style and serenity to the living room.",
@@ -15,7 +15,7 @@ const COLLECTIONS = [
   {
     id: 2,
     name: "2023 Collection",
-    imgSrc: "/images/2025-collection.jpg", // TODO: the correct image path
+    imgSrc: "/2023collectionImg.jpg", // TODO: the correct image path
     alt: "2023 Collection Image",
     description:
       "In 2023, we shifted our focus to the living room with our 2023 Collection. This collection was all about comfort and elegance, offering a range of modern, minimalist sofas, coffee tables, and storage pieces. With carefully selected fabrics, soft color palettes, and streamlined designs, the 2023 Collection transformed living rooms into luxurious, functional spaces where families could relax in style.",
@@ -24,7 +24,7 @@ const COLLECTIONS = [
   {
     id: 3,
     name: "2022 Collection",
-    imgSrc: "/images/2022-collection.jpg", // TODO: the correct image path
+    imgSrc: "/2022collectionImg.jpg", // TODO: the correct image path
     alt: "2022 Collection Image",
     description:
       "Building on the success of the previous year, our 2022 Collection further refined the art of minimalist kitchen furniture. We introduced new designs and finishes, with a focus on wood and metal accents that elevated the modern kitchen’s aesthetic. The collection included beautifully crafted dining tables, ergonomic chairs, and functional storage units, bringing both beauty and practicality to the kitchen space.",
@@ -33,7 +33,7 @@ const COLLECTIONS = [
   {
     id: 4,
     name: "2021 Collection",
-    imgSrc: "/images/2021-collection.jpg", // TODO: the correct image path
+    imgSrc: "/2021collectionImg.jpg", // TODO: the correct image path
     alt: "2021 Collection Image",
     description:
       "Our inaugural 2021 Collection brought a fresh perspective to kitchen furniture. Focusing on clean lines, functional design, and high-quality materials, this collection introduced sleek countertops, elegant dining sets, and space-saving storage solutions. Each piece was designed to create a minimalist yet inviting kitchen environment, perfect for modern homes.",
