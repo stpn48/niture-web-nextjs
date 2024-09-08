46116a289f463b8b10db3399c8d956cb9723685e
prdel24 <brozikmrozik7@gmail.com>
2024-09-08 18:11:16 +0200

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,10 +7,10 @@ export function HeroSection({}: Props) {
   return (
     <div className="flex flex-col items-center justify-center h-[800px]">
       <div className="flex flex-col gap-8 items-center">
-        <h1 className="font-bold text-[50px] w-[650px] text-center">
+        <h1 className="font-bold text-[50px] md:w-[650px] w-[50s0px] text-center">
           Elevate Your Space with Timeless Elegance
         </h1>
-        <h2 className="w-[650px] text-[18px] text-justify">
+        <h2 className="md:w-[650px] w-[400px] text-[18px] text-justify">
           We design and sell high-quality, minimalist furniture made from
           premium materials. From sleek living room pieces to elegant kitchen
           sets, our collections bring timeless style and comfort to your home.
