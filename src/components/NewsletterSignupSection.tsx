172351f1fce45a708eb57bfc4b78e8150e37fe13
prdel24 <brozikmrozik7@gmail.com>
2024-09-08 18:11:24 +0200

    Create NewsletterSignupSection.tsx

---
+++ src/components/NewsletterSignupSection.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button } from "./Button";
+
+type Props = {};
+
+export function NewsletterSignupSection({}: Props) {
+  return (
+    <div className="secondary-section-bg py-[60px] flex flex-col items-center gap-4">
+      <h1 className="text-lg font-bold">Sign Up for Our Newsletter</h1>
+      <h2 className="secondary-text-color mb-2">Stay Updated with Exclusive Offers and Design Inspiration.</h2>
+      <div>
+        <input
+          placeholder="Enter your email"
+          className="outline-none bg-white px-4 py-2"
+        ></input>
+        <Button>Sign up</Button>
+      </div>
+    </div>
+  );
+}
