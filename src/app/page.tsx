483a430f0e83eb840e239395ce520123a6e5c0bc
prdel24 <brozikmrozik7@gmail.com>
2024-09-08 18:10:42 +0200

    feat: add more secitons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import { AboutSection } from "@/components/AboutSection";
+import { ClientFeedbackSection } from "@/components/ClientFeedbackSection";
+import { CollectionsSection } from "@/components/CollectionsSection";
 import { FeaturedItems } from "@/components/FeaturedItems";
+import { Footer } from "@/components/Footer";
 import { HeroSection } from "@/components/HeroSection";
-import Image from "next/image";
+import { NewsletterSignupSection } from "@/components/NewsletterSignupSection";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <HeroSection />
       <FeaturedItems />
+      <CollectionsSection />
+      <AboutSection />
+      <ClientFeedbackSection />
+      <NewsletterSignupSection />
+      <Footer />
     </div>
   );
 }
