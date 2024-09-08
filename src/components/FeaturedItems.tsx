a1b86017df174a74362518ae4fc31ac76b70916a
prdel24 <brozikmrozik7@gmail.com>
2024-09-08 18:11:13 +0200

    Update FeaturedItems.tsx

--- src/components/FeaturedItems.tsx
+++ src/components/FeaturedItems.tsx
@@ -2,18 +2,17 @@ import React from "react";
 import { FeaturedItemsCarousel } from "./FeaturedItemsCarousel";
 import { storeItem } from "@/app/types";
 
-type Props = {};
-
-export async function FeaturedItems({}: Props) {
-
+export async function FeaturedItems() {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/featuredItems`
   );
   const featuredItems: storeItem[] = await res.json();
 
   return (
-    <div className="w-full bg-[#F7F7F7] py-[50px] px-20 overflow-hidden">
-      <h1 className="text-lg w-full flex justify-center">Featured Items</h1>
+    <div className="w-full secondary-section-bg py-[50px] overflow-hidden relative">
+      <h1 className="text-lg w-full flex justify-center mb-8">
+        Featured Items
+      </h1>
       <FeaturedItemsCarousel featuredItems={featuredItems} />
     </div>
   );
