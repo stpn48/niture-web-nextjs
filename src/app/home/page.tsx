4b6d7f404c9c97b922440e02043cb971d847f291
prdel24 <brozikmrozik7@gmail.com>
2024-09-09 19:19:25 +0200

    Create page.tsx

---
+++ src/app/home/page.tsx
@@ -0,0 +1,24 @@
+import { AboutSection } from "@/app/home/components/AboutSection";
+import { ClientFeedbackSection } from "@/app/home/components/ClientFeedbackSection";
+import { CollectionsSection } from "@/app/home/components/CollectionsSection";
+import { FeaturedItems } from "@/app/home/components/FeaturedItems";
+import { Footer } from "@/components/Footer";
+import { HeroSection } from "@/app/home/components/HeroSection";
+import { NewsletterSignupSection } from "@/app/home/components/NewsletterSignupSection";
+import { FadeAnimationWrapper } from "@/components/FadeAnimationWrapper";
+
+export default function HomePage() {
+  return (
+    <FadeAnimationWrapper>
+      <div className="min-h-screen">
+        <HeroSection />
+        <FeaturedItems />
+        <CollectionsSection />
+        <AboutSection />
+        <ClientFeedbackSection />
+        <NewsletterSignupSection />
+        <Footer />
+      </div>
+    </FadeAnimationWrapper>
+  );
+}
