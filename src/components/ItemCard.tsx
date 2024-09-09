ad0951c467776e6ff052da1b39df668b5fc1593d
prdel24 <brozikmrozik7@gmail.com>
2024-09-09 19:17:43 +0200

    Update ItemCard.tsx

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -4,15 +4,16 @@ import React from "react";
 
 export function ItemCard({ item }: { item: storeItem }) {
   return (
-    <div className="w-[350px] p-4 h-[400px] bg-white rounded-lg shadow-md">
-      <Image
-        width={1}
-        height={1}
-        className="h-full w-full"
-        objectFit="cover"
-        src={item.imgSrc}
-        alt={item.name}
-      />
+    <div className="w-[300px] shadow-spread p-4 h-[350px] bg-white rounded-lg">
+      <div className="w-full h-[190px] relative overflow-hidden">
+        <Image
+          layout="fill"
+          objectFit="cover"
+          className="w-full h-full"
+          src={item.imgSrc}
+          alt={item.name}
+        />
+      </div>
       <div className="mb-2">
         <h1 className="font-bold mb-1">{item.name}</h1>
         <p className="text-sm text-[#494949] text-justify">{item.description}</p>
