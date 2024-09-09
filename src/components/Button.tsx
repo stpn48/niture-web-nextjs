b6edc3fa7cbde84b2710b3200cbb2fab40fd7ada
prdel24 <brozikmrozik7@gmail.com>
2024-09-09 06:50:47 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,6 +11,7 @@ type Props = {
 export function Button({ children, onClick, className }: Props) {
   return (
     <button
+      onClick={onClick}
       className={`bg-[#262626] hover:bg-[#333333] active:bg-[#454545] text-white px-4 py-2 rounded-sm ${className}`}
     >
       {children}
