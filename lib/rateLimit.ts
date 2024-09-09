20e4d6caac71aaaea0b71b9525a5cbe3a01ea740
prdel24 <brozikmrozik7@gmail.com>
2024-09-09 06:51:08 +0200

    feat: create api rate limiter

--- lib/rateLimit.ts
+++ lib/rateLimit.ts
@@ -1,19 +1,23 @@
 // lib/rateLimit.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-import { RateLimiterMemory } from 'rate-limiter-flexible';
+import { NextApiRequest, NextApiResponse } from "next";
+import { RateLimiterMemory } from "rate-limiter-flexible";
 
 const rateLimiter = new RateLimiterMemory({
   points: 50, // Number of points
   duration: 60, // Per minute
 });
 
-export async function rateLimit(req: NextApiRequest, res: NextApiResponse, next: () => void) {
-  const ip = req.headers['x-forwarded-for'] as string || 'unknown';
+export async function rateLimit(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: () => void
+) {
+  const ip = (req.headers["x-forwarded-for"] as string) || "unknown";
 
   try {
     await rateLimiter.consume(ip);
     next();
   } catch (rejRes) {
-    res.status(429).json({ message: 'Too Many Requests' });
+    res.status(429).json({ message: "Too Many Requests" });
   }
 }
