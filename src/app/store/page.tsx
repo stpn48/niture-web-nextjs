cbf1eaaf48459820458e490996c6d09dd4f772c9
prdel24 <brozikmrozik7@gmail.com>
2024-09-10 07:41:16 +0200

    Update page.tsx

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -4,13 +4,14 @@ import { StoreItemsContainer } from "./components/StoreItemsContainer";
 import { storeItem } from "../types";
 
 async function fetchInitialItems(): Promise<storeItem[]> {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/storeItems?page=1&t=${Date.now()}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/storeItems?page=1&t=${Date.now()}`
+  );
   const items: storeItem[] = await res.json();
   return items;
 }
 
 export default async function Store() {
-
   const initialItems: storeItem[] = await fetchInitialItems();
 
   await new Promise<void>((resolve) => {
@@ -19,12 +20,17 @@ export default async function Store() {
     }, 1000);
   });
 
-
-
   return (
     <FadeAnimationWrapper>
       <div className="px-10 pt-16 bg-[#ffffff]">
         <h1 className="text-lg mb-5">Store</h1>
+        <div className="w-full flex justify-center">
+          <input
+            placeholder="Search"
+            className="outline-none text-center mb-4 bg-[#ffffff] px-4 py-1"
+            type="text"
+          />
+        </div>
         <div className="flex gap-8 min-h-screen">
           <div className="flex-1">
             <div className="w-full flex mb-10 items-center px-4 bg-[#f5f5f5] rounded-sm h-10">
