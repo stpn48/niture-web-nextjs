c64312792777772f059ac2c29fc8a84ded02638c
prdel24 <brozikmrozik7@gmail.com>
2024-09-11 18:26:04 +0200

    refactor: improve logic

--- src/app/store/components/StoreItems.tsx
+++ src/app/store/components/StoreItems.tsx
@@ -3,72 +3,25 @@
 import { ItemCard } from "@/components/ItemCard";
 import { LoadingBar } from "@/components/LoadingBar";
 import { useStoreItems } from "@/zustand/useStoreItems";
-import { throttle } from "lodash";
-import React, { useCallback, useEffect } from "react";
+import React from "react";
 
 const MemoItemCard = React.memo(ItemCard);
 
 export function StoreItems() {
-  const {
-    items,
-    loading,
-    query,
-    hasMoreItems,
-    page,
-    setLoading,
-    setItems,
-    setPage,
-    setHasMoreItems,
-  } = useStoreItems();
 
-  const fetchMoreItems = useCallback(async (pageNumber: number) => {
-    try {
-      setLoading(true);
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/storeItems?page=${pageNumber}&t=${Date.now()}`);
-      const newItems = await res.json();
-
-      if (newItems.length === 0) {
-        setHasMoreItems(false);
-      } else {
-        setItems((prevItems) => [...prevItems, ...newItems]);
-        setPage((prevPage) => prevPage + 1);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
-  // handle infinite scroll
-  useEffect(() => {
-    const handleScroll = throttle(async () => {
-      if (query) return;
-
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        if (hasMoreItems && !loading) {
-          await fetchMoreItems(page);
-        }
-      }
-    }, 200); // Throttle the scroll event to fire every 200ms
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [page, fetchMoreItems, hasMoreItems, loading, query]);
+  const {items, loading} = useStoreItems();
 
 
+  // TODO: Implement infinite scroll
   return (
     <>
-      <div className="flex flex-wrap gap-10 mb-10 justify-center">
+      <div className="mb-10 flex flex-wrap justify-center gap-10">
         {items.map((item) => (
           <MemoItemCard key={item.id} item={item} />
         ))}
       </div>
       {loading && (
-        <div className="w-full flex justify-center">
+        <div className="flex w-full justify-center">
           <LoadingBar className="my-10" />
         </div>
       )}
