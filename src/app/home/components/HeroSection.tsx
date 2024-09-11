aea85bccb940d45d669e7e20b768051ad4e4bab7
prdel24 <brozikmrozik7@gmail.com>
2024-09-11 18:24:54 +0200

    Update HeroSection.tsx

--- src/app/home/components/HeroSection.tsx
+++ src/app/home/components/HeroSection.tsx
@@ -7,7 +7,6 @@ import { useRouter } from "next/navigation";
 type Props = {};
 
 export function HeroSection({}: Props) {
-
   const router = useRouter();
 
   function shopNowOnClick() {
@@ -15,12 +14,12 @@ export function HeroSection({}: Props) {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-[800px]">
-      <div className="flex flex-col gap-8 items-center">
-        <h1 className="font-bold text-[50px] md:w-[650px] w-[500px] text-center">
+    <div className="flex h-[800px] flex-col items-center justify-center">
+      <div className="flex flex-col items-center gap-8">
+        <h1 className="w-[500px] text-center text-[50px] font-bold md:w-[650px]">
           Elevate Your Space with Timeless Elegance
         </h1>
-        <h2 className="md:w-[650px] w-[400px] text-[18px] text-justify">
+        <h2 className="w-[400px] text-justify text-[18px] md:w-[650px]">
           We design and sell high-quality, minimalist furniture made from
           premium materials. From sleek living room pieces to elegant kitchen
           sets, our collections bring timeless style and comfort to your home.
