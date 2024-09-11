98bcd83560874f1885ec303039ee29f4f2d418aa
prdel24 <brozikmrozik7@gmail.com>
2024-09-11 18:26:48 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,9 +18,11 @@ const navLinks = [
 
 export function Navbar({ navLinksVisible = true }: Props) {
   return (
-    <div className="bg-white px-5 fixed z-10 py-2 justify-between h-fit w-full flex items-center">
-      <Link href={"/"} className="text-lg cursor-pointer">Niture</Link>
-      <div className="gap-4 flex items-center">
+    <div className="fixed z-10 flex h-fit w-full items-center justify-between bg-white px-5 py-2">
+      <Link href={"/"} className="cursor-pointer text-lg">
+        Niture
+      </Link>
+      <div className="flex items-center gap-4">
         {navLinksVisible &&
           navLinks.map((link) => (
             <Link
@@ -31,8 +33,8 @@ export function Navbar({ navLinksVisible = true }: Props) {
               {link.name}
             </Link>
           ))}
-        <h1 className="hover:text-[#9c7a54] cursor-pointer">Cart</h1>
-        <img src="/menuImg.svg" alt="logo" className="w-4 h-3 cursor-pointer" />
+        <h1 className="cursor-pointer hover:text-[#9c7a54]">Cart</h1>
+        <img src="/menuImg.svg" alt="logo" className="h-3 w-4 cursor-pointer" />
       </div>
     </div>
   );
