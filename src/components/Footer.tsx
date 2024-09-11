c8eb73dfa9970eb121d299488339897bc85aff3c
prdel24 <brozikmrozik7@gmail.com>
2024-09-11 18:26:40 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ type Props = {};
 
 export function Footer({}: Props) {
   return (
-    <div className="bg-[#1A1A1A] py-10 px-14 text-white w-full h-[400px]">
-      <div className="flex gap-2 items-center">
+    <div className="h-[400px] w-full bg-[#1A1A1A] px-14 py-10 text-white">
+      <div className="flex items-center gap-2">
         <h1 className="text-lg">Niture</h1>
         <a href="https://x.com" target="_blank">
           <Image
@@ -18,10 +18,10 @@ export function Footer({}: Props) {
           />
         </a>
       </div>
-      <div className="flex mt-12 justify-between items-center">
-        <div className="md:block hidden">
-          <h2 className="underline text-sm mb-2">About us</h2>
-          <p className="text-[#cccccc] text-sm w-[350px] text-justify ">
+      <div className="mt-12 flex items-center justify-between">
+        <div className="hidden md:block">
+          <h2 className="mb-2 text-sm underline">About us</h2>
+          <p className="w-[350px] text-justify text-sm text-[#cccccc]">
             Founded in 2017, we began with a mission to create high-quality,
             functional furniture. Our initial focus on durable basics evolved
             into a brand known for minimalist design and superior craftsmanship.
@@ -31,7 +31,7 @@ export function Footer({}: Props) {
             simplicity, and timeless beauty.
           </p>
         </div>
-        <div className="lg:block hidden">
+        <div className="hidden lg:block">
           <Image
             src="/NitureSignature.png"
             alt="Niture Signature"
@@ -40,7 +40,7 @@ export function Footer({}: Props) {
           />
         </div>
         <div className="text-[#cccccc]">
-          <h2 className="text-sm underline text-white mb-2">Contact us</h2>
+          <h2 className="mb-2 text-sm text-white underline">Contact us</h2>
           <div className="flex flex-col gap-2 text-sm">
             <p>info@niture.com</p>
             <p>+1 (555) 555-5555</p>
