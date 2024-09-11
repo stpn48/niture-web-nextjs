73d80d644a765cde081a70777b5723c6deef2ebd
prdel24 <brozikmrozik7@gmail.com>
2024-09-11 18:26:31 +0200

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,13 +6,19 @@ type Props = {
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
+  disabled?: boolean;
 };
 
-export function Button({ children, onClick, className }: Props) {
+export function Button({ children, onClick, className, disabled }: Props) {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
-      className={`bg-[#262626] hover:bg-[#333333] active:bg-[#454545] text-white px-4 py-2 rounded-sm ${className}`}
+      className={`${
+        disabled
+          ? "cursor-not-allowed bg-[#3f3f3f]"
+          : "bg-[#262626] hover:bg-[#333333] active:bg-[#454545]"
+      } px-4 py-2 text-white ${className}`}
     >
       {children}
     </button>
