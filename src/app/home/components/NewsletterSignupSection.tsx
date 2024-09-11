03f70624e1178002c48afb53066005a6a58a49a2
prdel24 <brozikmrozik7@gmail.com>
2024-09-11 18:24:56 +0200

    Update NewsletterSignupSection.tsx

--- src/app/home/components/NewsletterSignupSection.tsx
+++ src/app/home/components/NewsletterSignupSection.tsx
@@ -5,13 +5,15 @@ type Props = {};
 
 export function NewsletterSignupSection({}: Props) {
   return (
-    <div className="secondary-section-bg py-[60px] flex flex-col items-center gap-4">
+    <div className="secondary-section-bg flex flex-col items-center gap-4 py-[60px]">
       <h1 className="text-lg font-bold">Sign Up for Our Newsletter</h1>
-      <h2 className="secondary-text-color mb-2">Stay Updated with Exclusive Offers and Design Inspiration.</h2>
+      <h2 className="secondary-text-color mb-2">
+        Stay Updated with Exclusive Offers and Design Inspiration.
+      </h2>
       <div>
         <input
           placeholder="Enter your email"
-          className="outline-none bg-white px-4 py-2"
+          className="bg-white px-4 py-2 outline-none"
         ></input>
         <Button>Sign up</Button>
       </div>
