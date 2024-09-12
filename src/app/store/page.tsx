39cdb2927e5c2f700bb65261382735457044d2ed
prdel24 <brozikmrozik7@gmail.com>
2024-09-12 06:52:48 +0200

    Update page.tsx

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,70 +1,57 @@
 "use client";
 
-import React, { useEffect } from "react";
 import { FadeAnimationWrapper } from "@/components/FadeAnimationWrapper";
 import { SearchBar } from "@/app/store/components/SearchBar";
 import { StoreItems } from "@/app/store/components/StoreItems";
 import { FilterBar } from "@/app/store/components/FilterBar";
-import { useStoreItems } from "@/zustand/useStoreItems";
+import { fetchStoreItems } from "@/server/actions/storeActions";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Store() {
-  const { activeTags, query, setItems, setLoading, setQuery, setActiveTags } =
-    useStoreItems();
+  const userSearchParams = useSearchParams();
+  const [query, setQuery] = useState(userSearchParams.get("q") || "");
+  const [activeTags, setActiveTags] = useState<string[]>(
+    JSON.parse(userSearchParams.get("tags") || "[]"),
+  );
 
-  // TODO: Implement on mount and on query change
+  // Use effect to detect URL changes and update state accordingly
   useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const queryParam = searchParams.get("q") || "";
-    const tagsParam = searchParams.get("tags")
-      ? JSON.parse(searchParams.get("tags") || "[]")
-      : [];
+    const newQuery = userSearchParams.get("q") || "";
+    const newTags = JSON.parse(userSearchParams.get("tags") || "[]");
 
-    setQuery(queryParam);
-    setActiveTags(tagsParam);
-  }, []);
+    setQuery(newQuery);
+    setActiveTags(newTags);
+  }, [userSearchParams]);
 
-  useEffect(() => {
-    async function fetchItems() {
-      setLoading(true);
-      const queryParams = new URLSearchParams();
-
-      if (query) {
-        queryParams.append("q", query);
-      }
-
-      if (activeTags.length !== 0) {
-        queryParams.append("tags", JSON.stringify(activeTags));
-      }
-
-      const url = `${process.env.NEXT_PUBLIC_API_URL}/store?${queryParams.toString()}`;
-      window.history.pushState({}, "", url);
-
-      try {
-        const res = await fetch(
-          `${process.env.NEXT_PUBLIC_API_URL}/api/storeItems?${queryParams.toString()}`,
-        );
-
-        if (!res.ok) throw new Error("Network response was not ok");
-
-        const items = await res.json();
-        setItems(items);
-      } catch (error) {
-        console.error("Failed to fetch items:", error);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchItems();
-  }, [activeTags, query, setItems, setLoading]);
+  const {
+    data: items,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["storeItems", query, activeTags],
+    queryFn: async () => {
+      const items = await fetchStoreItems(query, activeTags);
+      return items;
+    },
+    // Ensures that the query runs whenever query or activeTags change
+    enabled: !!query || activeTags.length > 0,
+  });
 
   return (
     <FadeAnimationWrapper>
       <div className="bg-[#ffffff] px-10 pt-16">
         <div className="flex flex-col items-center">
-          <SearchBar />
-          <FilterBar />
-          <StoreItems />
+          <SearchBar query={query} />
+          <FilterBar activeTags={activeTags} />
+          {isLoading ? (
+            <p>Loading...</p>
+          ) : isError ? (
+            <p>Error loading items</p>
+          ) : (
+            <StoreItems items={items || []} />
+          )}
         </div>
       </div>
     </FadeAnimationWrapper>
