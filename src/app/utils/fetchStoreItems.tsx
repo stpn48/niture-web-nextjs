7576ac0f9f0485b689f53c0876ca30c235f8cb8d
prdel24 <brozikmrozik7@gmail.com>
2024-09-13 19:09:02 +0200

    Create fetchStoreItems.tsx

---
+++ src/app/utils/fetchStoreItems.tsx
@@ -0,0 +1,21 @@
+import { storeItem } from "../types";
+
+export async function fetchStoreItems(
+  params: string,
+): Promise<{ items: storeItem[]; nextCursor: number | null }> {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/storeItems?${params}`,
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch items");
+    }
+    const data = await res.json();
+    return data;
+
+  } catch (error) {
+    console.error(error);
+    return { items: [], nextCursor: null };
+  }
+}
