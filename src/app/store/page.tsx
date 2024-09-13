1f6bd2cae20f06a60c4d471381dfe79b851efc64
prdel24 <brozikmrozik7@gmail.com>
2024-09-13 19:09:04 +0200

    Update page.tsx

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,57 +1,20 @@
-"use client";
-
 import { FadeAnimationWrapper } from "@/components/FadeAnimationWrapper";
 import { SearchBar } from "@/app/store/components/SearchBar";
 import { StoreItems } from "@/app/store/components/StoreItems";
 import { FilterBar } from "@/app/store/components/FilterBar";
-import { fetchStoreItems } from "@/server/actions/storeActions";
-import { useQuery } from "@tanstack/react-query";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { fetchStoreItems } from "../utils/fetchStoreItems";
 
-export default function Store() {
-  const userSearchParams = useSearchParams();
-  const [query, setQuery] = useState(userSearchParams.get("q") || "");
-  const [activeTags, setActiveTags] = useState<string[]>(
-    JSON.parse(userSearchParams.get("tags") || "[]"),
-  );
+export default async function Store() {
 
-  // Use effect to detect URL changes and update state accordingly
-  useEffect(() => {
-    const newQuery = userSearchParams.get("q") || "";
-    const newTags = JSON.parse(userSearchParams.get("tags") || "[]");
-
-    setQuery(newQuery);
-    setActiveTags(newTags);
-  }, [userSearchParams]);
-
-  const {
-    data: items,
-    isError,
-    isLoading,
-  } = useQuery({
-    queryKey: ["storeItems", query, activeTags],
-    queryFn: async () => {
-      const items = await fetchStoreItems(query, activeTags);
-      return items;
-    },
-    // Ensures that the query runs whenever query or activeTags change
-    enabled: !!query || activeTags.length > 0,
-  });
+  const initialItems = await fetchStoreItems("page=1");
 
   return (
     <FadeAnimationWrapper>
       <div className="bg-[#ffffff] px-10 pt-16">
         <div className="flex flex-col items-center">
-          <SearchBar query={query} />
-          <FilterBar activeTags={activeTags} />
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : isError ? (
-            <p>Error loading items</p>
-          ) : (
-            <StoreItems items={items || []} />
-          )}
+          <SearchBar/>
+          <FilterBar />
+          <StoreItems initialItems={initialItems.items} />
         </div>
       </div>
     </FadeAnimationWrapper>
