43cb7c27c6bf5b808b41c641ae556cb4ad12cef0
prdel24 <brozikmrozik7@gmail.com>
2024-09-15 11:06:04 +0200

    Update cartStore.tsx

--- src/zustand/cartStore.tsx
+++ src/zustand/cartStore.tsx
@@ -4,9 +4,12 @@ import { create } from "zustand";
 type Store = {
   cartItems: cartItem[];
   setCartItems: (val: cartItem[] | ((prev: cartItem[]) => cartItem[])) => void;
+
   getCartItem: (id: number) => cartItem | undefined;
   isCartOpen: boolean;
   setIsCartOpen: (val: boolean) => void;
+
+  updateItemQuantity: (id: number, quantity: number) => void;
 };
 
 export const useCartStore = create<Store>((set, get) => ({
@@ -21,7 +24,28 @@ export const useCartStore = create<Store>((set, get) => ({
     const state = get();
     return state.cartItems.find((item) => item.id === id);
   },
-  
+
   isCartOpen: false,
   setIsCartOpen: (val) => set({ isCartOpen: val }),
+
+  updateItemQuantity: (id, quantity) => {
+    const state = get();
+
+    const itemIndexToUpdate = state.cartItems.findIndex(
+      (item) => item.id === id,
+    );
+
+    if (itemIndexToUpdate === -1) {
+      return;
+    }
+
+    const updatedCartItems = [...state.cartItems];
+
+    updatedCartItems[itemIndexToUpdate] = {
+      ...state.cartItems[itemIndexToUpdate],
+      quantity: quantity,
+    };
+
+    set({ cartItems: updatedCartItems });
+  },
 }));
